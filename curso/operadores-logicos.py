a2880ec9b5301ac054bfd47d0f28be117d7ef33a
Guilherme Kindlmann Vilela <guilherme.vilela@matera.com>
2022-01-27 12:18:42 -0300

    curso1

---
+++ curso/operadores-logicos.py
@@ -0,0 +1,8 @@
+x = 2
+y = 3
+z = 3
+
+#print(x == y and x == z)
+print(x == y or y == z)
+#print(x == y and x == z)
+#print(x == y and x == z)
---
+++ curso/tipos.py
@@ -0,0 +1,16 @@
+#variavel inteira
+var1 = 1 
+#variavel float
+var2 = 1.1
+#variavel string
+var3 = 'Eu sou string'
+# booleana
+var4 = true
+var4 = false
+
+#print (var1)
+#print (var2)
+#print (var3)
+print (var4)
+
+
